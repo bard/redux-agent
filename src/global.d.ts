422c2322a7795e3171beee8fcca713845dbdccd2
Massimiliano Mirra <bard@hyperstruct.net>
2019-03-06 19:14:08 +0100

    chore: workaround for rollup importing @types/node

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,34 +1,21 @@
 declare type JSONType = import('./types').JSONObject
 
 
-// Temporary. Should be removed after @types/node is no longer
-// imported into global scope by transitive dependencies, and replaced
-// with:
-//
-// declare module module {
-//   export const hot: any
-// }
-
-interface NodeModule {
-  hot: any
+declare module module {
+  export const hot: any
 }
 
 declare interface WebSocket {
   sendJSON(data: any): void
 }
 
-
 // Workaround for using immer in projects targeting ES5.
 // https://github.com/mweststrate/immer/issues/321
-//
-// Temporarily commented out, because @types/node is being imported by
-// transitive dependencies. Must be uncommented again when that is no
-// longer the case.
-//
-// declare interface Map<K, V> {}
-// declare interface WeakMap<K, V> {}
-// declare interface WeakSet<K> {}
-// declare interface Set<K> {}
+
+declare interface Map<K, V> {}
+declare interface WeakMap<K, V> {}
+declare interface WeakSet<K> {}
+declare interface Set<K> {}
 
 // Polyfills
 
