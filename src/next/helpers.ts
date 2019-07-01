baabbc8c41dac1ea51d0e980bac48f16e80fbcc2
Massimiliano Mirra <bard@hyperstruct.net>
2019-07-01 20:43:21 +0200

    feat: use immer transparently

--- src/next/helpers.ts
+++ src/next/helpers.ts
@@ -1,13 +1,14 @@
 import invariant from 'invariant'
+import withImmer from '../util/with-immer'
 import { Task, SystemTask } from './types'
 
 export const addTask = <T extends Task>(
   // the 'tasks' state member is added at runtime, we don't want
   // to enforce a type because the developer should not be concerned
   // with how the state object is made to support a task store
-  draft: any,
+  state: any,
   task: T
-) => {
+) => withImmer(state, (draft: any) => {
   invariant(draft.tasks,
     'State not initialized for Redux Agent. ' +
     'Did you call reduceReducers(..., taskReducer)?')
@@ -16,15 +17,15 @@ export const addTask = <T extends Task>(
   systemTask.nextTaskId += 1
 
   draft.tasks[systemTask.nextTaskId] = task
-}
+})
 
 export const delTasks = <T extends Task>(
-  draft: any,
+  state: any,
   taskFilter: (t: T) => boolean
-) => {
+) => withImmer(state, (draft: any) => {
   for (const tid in draft.tasks) {
     if (taskFilter(draft.tasks[tid])) {
       delete draft.tasks[tid]
     }
   }
-}
+})
