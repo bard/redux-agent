b162ebc6e07c65dbbd433a57a766ede11f548fcf
Massimiliano Mirra <bard@hyperstruct.net>
2019-07-01 19:58:46 +0200

    refactor: improve helper typing

--- src/next/helpers.ts
+++ src/next/helpers.ts
@@ -1,26 +1,30 @@
-import { Task, SystemTask, State } from './types'
 import invariant from 'invariant'
+import { Task, SystemTask } from './types'
+
+export const addTask = <T extends Task>(
+  // the 'tasks' state member is added at runtime, we don't want
+  // to enforce a type because the developer should not be concerned
+  // with how the state object is made to support a task store
+  draft: any,
+  task: T
+) => {
   invariant(draft.tasks,
     'State not initialized for Redux Agent. ' +
     'Did you call reduceReducers(..., taskReducer)?')
 
-export const addTask = <
-  S extends State,
-  T extends Task
->(draft: S, task: T) => {
   const systemTask = (draft.tasks[0] as SystemTask)
   systemTask.nextTaskId += 1
 
   draft.tasks[systemTask.nextTaskId] = task
 }
 
-export const delTasks = <
-  S extends State
->(draft: S, taskFilter: (t: Task) => boolean) => {
-  for (const [tid, task] of Object.entries(draft.tasks)) {
-    if (taskFilter(task)) {
+export const delTasks = <T extends Task>(
+  draft: any,
+  taskFilter: (t: T) => boolean
+) => {
+  for (const tid in draft.tasks) {
+    if (taskFilter(draft.tasks[tid])) {
       delete draft.tasks[tid]
     }
   }
 }
-
